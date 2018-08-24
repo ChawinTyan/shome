430d88c38955da0414eaab108faa0bb85b7d81fa
apollo <chawintyan@aliyun.com>
2018-08-24 10:39:02 +0800

    更新JAR包引用

--- shome-web/src/main/java/web/spring/ActiveMQConfig.java
+++ shome-web/src/main/java/web/spring/ActiveMQConfig.java
@@ -2,7 +2,6 @@ package web.spring;
 
 import org.apache.activemq.ActiveMQConnectionFactory;
 import org.apache.activemq.command.ActiveMQQueue;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
